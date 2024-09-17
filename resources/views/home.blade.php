01e2b280f5f8b4c7c020e736ba8c5563be128973
christetelyou <christelmaeamante@gmail.com>
2024-09-17 21:53:20 +0800

    added small changes to css box shadow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,7 +27,8 @@
         padding: 20px;
         background-color: #f6f7f6;
         border-radius: 5px;
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px -1px rgba(0, 0, 0, 0.1);
+        /* box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px -1px rgba(0, 0, 0, 0.1); */
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 2px 2px rgba(0, 0, 0, 0.12);
     }
 
     .home-container h2 {
